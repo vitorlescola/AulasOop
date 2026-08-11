219ec55ce7c473e0285a22780bf1d4a766665c2b
Vitor Luis Nonino <158179013+vitorlescola@users.noreply.github.com>
2026-08-10 22:16:06 -0300

    Add files via upload

---
+++ Aula02/src/main/java/br/ed/unicesumar/Main.java
@@ -0,0 +1,16 @@
+package br.ed.unicesumar;
+
+public class Main {
+    static void main() {
+        Veiculo uno=new Veiculo("","",0);
+
+        uno.setMarca("Fiat");
+        uno.setModelo("Uno");
+        uno.setAno(2009);
+
+        uno.exibirDados();
+
+        Veiculo fusca=new Veiculo("WV","Fusca",1976);
+        fusca.exibirDados();
+    }
+}
---
+++ Aula02/src/main/java/br/ed/unicesumar/Veiculo.java
@@ -0,0 +1,52 @@
+package br.ed.unicesumar;
+
+public class Veiculo {
+    private int id;
+    private String marca;
+    private String modelo;
+    private int ano;
+    private int qtdPortas;
+
+    public Veiculo(String marca, String modelo, int ano){
+        this.marca=marca;
+        this.modelo=modelo;
+        this.ano=ano;
+        this.qtdPortas=2;
+    }
+
+    public void exibirDados(){
+        System.out.printf("Marca:%s\nModelo:%s\nAno:%d\nQuantidade de portas:%d",marca,modelo,ano,qtdPortas);
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public int getQtdPortas() {
+        return qtdPortas;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void setQtdPortas(int qtdPortas) {
+        this.qtdPortas = qtdPortas;
+    }
+}
